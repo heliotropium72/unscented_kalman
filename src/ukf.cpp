bc5d6726315b94c91035b2386ab47d91cfd2f55e
Solvejg <asd@nilu.no>
2018-02-20 17:24:21 +0100

    Tune Parameters3

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -35,10 +35,10 @@ UKF::UKF() {
 
   // TUNE THESE TWO NOISE PARAMETERS !!!
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 2;
+  std_a_ = 1;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 0.5;
+  std_yawdd_ = 0.8;
   
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
